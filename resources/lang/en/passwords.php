ae052272088bed05fdd20a62182939d7fc1f03ca
fuyuooumi1027 <fuyuooumi1027@gmail.com>
2023-12-22 15:29:22 -0800

    admin

---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'reset_link_sent' => 'パスワード リセット リンクが正常に送信されました。',
+    'user_not_found' => "そのメール アドレスを持つユーザーが見つかりません。",
+    // other language keys...
+];
--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -11,17 +11,19 @@
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <form class="form text-center" method="POST" action="{{ route('login') }}">
+        <form class="form text-center" method="POST" action="{{ route('password.email') }}">
             @csrf
             <div class="form-group">
                 <input class="form-control h-auto text-white placeholder-white opacity-70 bg-dark-o-70 rounded-pill border-0 py-4 px-8 p-form-control" id="email" type="email" placeholder="メールアドレス" name="email" required autofocus />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
+            
+            <div class="form-group">
+                <button type="submit" class="btn btn-pill btn-outline-white font-weight-bold opacity-90 px-15 py-3 m-2 p-primary-btn">送信する</button>
+                <a class="btn btn-pill btn-outline-white font-weight-bold opacity-70 px-15 py-3 m-2" href="/login">キャンセル</a>
+            </div>
         </form>
-        <div class="form-group">
-            <button type="submit" class="btn btn-pill btn-outline-white font-weight-bold opacity-90 px-15 py-3 m-2 p-primary-btn">送信する</button>
-            <a class="btn btn-pill btn-outline-white font-weight-bold opacity-70 px-15 py-3 m-2" href="/login">キャンセル</a>
-        </div>
+        
     </div>
     <!--end::Login Sign in form-->
 @endsection
